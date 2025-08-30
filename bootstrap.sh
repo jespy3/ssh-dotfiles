20a1e4d2c590f6d6b9f2353163582731fcf56f35
jespy3 <29829925+jespy3@users.noreply.github.com>
2025-08-30 14:41:41 +1200

    fix: avoid recursive symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,12 @@ export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 mkdir -p $XDG_CONFIG_HOME
 
 # Symlink config dirs
-ln -sf "$PWD/.config/bash" "$XDG_CONFIG_HOME/bash"
-ln -sf "$PWD/.config/git" "$XDG_CONFIG_HOME/git"
-ln -sf "$PWD/.config/tmux" "$XDG_CONFIG_HOME/tmux"
-ln -sf "$PWD/.vimrc" ~/.vimrc
+ln -nsf "$PWD/.config/bash" "$XDG_CONFIG_HOME/bash"
+ln -nsf "$PWD/.config/git" "$XDG_CONFIG_HOME/git"
+ln -nsf "$PWD/.config/tmux" "$XDG_CONFIG_HOME/tmux"
+ln -nsf "$PWD/.vimrc" ~/.vimrc
 
 # source bash shell
-ln -sf "$PWD/.bashrc" ~/.bashrc
+ln -nsf "$PWD/.bashrc" ~/.bashrc
 source ~/.bashrc
 
